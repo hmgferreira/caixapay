f1aa9923deae37a6eb37610a9a40a0d61e9a264b
wuller Barros <liviaxavier200317@gmail.com>
2023-08-16 20:26:33 -0300

    Ajustes

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Api from '../../config/Api';
 import { useNavigate } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function Login() {
-
     const[login, setLogin] = React.useState('');
     const[senha, setSenha] = React.useState('');
     const navigate = useNavigate();
@@ -24,11 +24,24 @@ function Login() {
     }
 
     return (
-        <>
-            <input type="text" onChange={e => setLogin(e.target.value)}/>
-            <input type="password" onChange={e => setSenha(e.target.value)} />
-            <button onClick={logar}>Acessar</button>
-        </>
+        <Container>
+            <Row>
+                <Col md={{ span: 4, offset: 4 }}>
+                    <div className='mt-3'>
+                        <label>Login</label>
+                        <input className='form-control' type="text" onChange={e => setLogin(e.target.value)}/>
+                    </div>
+                    <div className='mt-3'>
+                        <label>Senha</label>
+                        <input className='form-control' type="password" onChange={e => setSenha(e.target.value)} />
+                    </div>
+                    <div className='mt-3'>
+                        <button className='btn btn-success btn-sm' onClick={logar}>Acessar</button>
+                    </div>
+
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
